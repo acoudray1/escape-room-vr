378b5419d80771dc9f5d74acfad205ff91e7445d
acoudray1 <axel.hpm.coudray@gmail.com>
2020-11-09 11:52:27 +0100

    corrected ovr plugin nullpointerexception

--- Assets/Oculus/VR/Scripts/OVRPlugin.cs
+++ Assets/Oculus/VR/Scripts/OVRPlugin.cs
@@ -38,6 +38,9 @@ using UnityEngine;
 
 public static class OVRPlugin
 {
+private const string pluginName = "OVRPlugin";
+private static System.Version _versionZero = new System.Version(0, 0, 0);
+
 #if OVRPLUGIN_UNSUPPORTED_PLATFORM
 	public const bool isSupportedPlatform = false;
 #else
@@ -4775,9 +4778,6 @@ public static class OVRPlugin
 		}
 #endif
 	}
-
-	private const string pluginName = "OVRPlugin";
-	private static System.Version _versionZero = new System.Version(0, 0, 0);
 
 	// Disable all the DllImports when the platform is not supported
 #if !OVRPLUGIN_UNSUPPORTED_PLATFORM
